67e4880d7b87e1e4f5d4e817d580cb38430cf997
Dhanush S <dhanushdh01@gmail.com>
2024-10-03 10:37:53 +0530

    Added

--- src/main/java/com/dhanush/Main.java
+++ src/main/java/com/dhanush/Main.java
@@ -4,17 +4,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        PaytmFlightScraper scraper = new PaytmFlightScraper();
-//        try {
-//            List<String> flightDetails = scraper.scrapeFlightDetails();
-//            System.out.println("Flight search results:");
-//            for (String details : flightDetails) {
-//                System.out.println(details);
-//            }
-//            CSVUtils.saveToCSV(flightDetails, "flight_details.csv");
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        PaytmFlightScraper scraper = new PaytmFlightScraper();
+        try {
+            List<String> flightDetails = scraper.scrapeFlightDetails();
+            System.out.println("Flight search results:");
+            for (String details : flightDetails) {
+                System.out.println(details);
+            }
+            CSVUtils.saveToCSV(flightDetails, "flight_details.csv");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         ClearTripFlightScraper clearTripFlightScraper = new ClearTripFlightScraper();
         try {
